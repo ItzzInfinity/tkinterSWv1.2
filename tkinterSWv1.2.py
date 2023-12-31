7f5f465b985b0d465567676a2bdf5fb2cb1355f2
Anjan Prasad <97271459+ItzzInfinity@users.noreply.github.com>
2023-12-31 19:45:22 +0530

    Update tkinterSWv1.2.py

--- tkinterSWv1.2.py
+++ tkinterSWv1.2.py
@@ -54,8 +54,6 @@ def on_button_click(store):
     messagebox.showinfo("SIS Report Downloader says", f"Wait for few seconds {store} Report is downloading")
     
 def on_date_pick():
-    # picked_date = date_picker.get()
-    # print(f"Picked Date: {picked_date}")
     print(cal.get_date())
 
 def seleniumLoginTry(store):   
@@ -144,7 +142,6 @@ def excelCombine():  #some time combines sometime not try to resolve
 
 def AfterDownloadMoveToFolder():
     folder_path = 'C:\\Users\\ANJAN\\Downloads'
-    # newPath = 'C:\\Users\\ANJAN\\Downloads\\2023-06-09'
     file_type = r'\*csv'
 
     td = date.today() # Use the today method and assign it to the td variable.  
@@ -181,19 +178,15 @@ def sotreDataWithForLoop():
     driver = webdriver.Chrome(options=options, service=Service("C:/chromedriver_win32/chromedriver.exe"))
     #open the webpage
     driver.get("https://ltesd.telesonic.in:8443/store/login")
-    #     store_names = [
-    #    "Asansol","Baharampore", "Haldia","Howrah", "Jalpaiguri",
-    #     "Kalyani", "Kharagpur", "Mukandapur", "New Alipur", "Port Blair",
-    #     "Rajarhat","Ranaghat/Park Circus", "Nimta", "Salt Lake", "Siliguri",  
-    # ]
+
     SIS_ID =    [ 
-            "SDAS-11","IHNAB04","IHNFH02","BCHAT02","ACHAK02",
-            "JMAJU03","SPKUM02","PUSHP03","ASIL-01 ","BGIRI01",
-            "TDAS01","RDEBN01","NGHOS01","AGHOS02","ASAH-01"]
+             "******","******","******","******","******", #id s are hidden for security purpose
+             "******","******","******","******","******",
+             "******","******","******","******","******",]
     SIS_PASSWORD =  [
-            "Air@S77857","AIR@NA7128","AIR@NF4596","Air@K59380","Air@M11211",
-            "Air@J95342","Air@UM7550","Air@P54824","Air@A69931","AIR@BG6137",
-            "AIR@TD9338","Air@P62212","Air@R33447","Air@A34500","Air@A41439"]
+            "******","******","******","******","******", #passwords as well
+             "******","******","******","******","******",
+             "******","******","******","******","******",]
 
     for i in range(15):
         # find username/email field and send the username itself to the input field
@@ -208,8 +201,7 @@ def sotreDataWithForLoop():
         #wait until the login is done
 
         t.sleep(3)
-        # element = WebDriverWait(ChromeDriverManager, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Logout")))
-        
+       
         driver.get(downloadUrl)
         
         #wait until the DOWNLOAD is done
@@ -218,19 +210,6 @@ def sotreDataWithForLoop():
         # Logging out
         driver.find_element(By.PARTIAL_LINK_TEXT,"Logout").click()
     driver.close()
-
-#from Stackover flow to make exe from it
-# def resource_path(relative_path):
-#     """ Get absolute path to resource, works for dev and for PyInstaller """
-#     try:
-#         # PyInstaller creates a temp folder and stores path in _MEIPASS
-#         base_path = sys._MEIPASS
-#     except Exception:
-#         base_path = os.path.abspath(".")
-
-#     return os.path.join(base_path, relative_path)
-
-
 
 
 root = tk.Tk()
@@ -248,32 +227,15 @@ store_names = [
     "Kalyani", "Kharagpur", "Mukandapur", "New Alipur", "Port Blair",
     "Rajarhat","Ranaghat/Park Circus", "Nimta", "Salt Lake", "Siliguri",  
 ]
-# 1	ASANSOL	                    SDAS-11	    Air@S77857
-# 2	BAHARAMPORE              	IHNAB04	    AIR@NA7128
-# 3	HALDIA	                    IHNFH02	    AIR@NF4596
-# 4	HOWRAH	                    BCHAT02	    Air@K59380
-# 5	JALPAIGURI	                ACHAK02	    Air@M11211
-# 6	KALYANI	                    JMAJU03	    Air@J95342
-# 7	KHARAGPUR	                SPKUM02	    Air@UM7550
-# 8	MUKANDAPUR	                PUSHP03	    Air@P54824
-# 9	NEW ALIPUR	                ASIL-01 	Air@A69931
-# 10 PORT BLAIR	                BGIRI01	    AIR@BG6137
-# 11 RAJARHAT	                TDAS01	    AIR@TD9338
-# 12 RANAGHAT	                RDEBN01  	Air@P62212
-# 13 RISHRA	                    NGHOS01	    Air@R33447
-# 14 SALT LAKE	                AGHOS02	    Air@A34500
-# 15 SILIGURI	                ASAH-01	    Air@A41439 
-## RANAGHAT REMOVED NEED TO BE ADDED // AS THE USERID/ PWD WAS WRONG
-# STORE_NAME = 	{'ASANSOL','BAHARAMPORE','HALDIA','HOWRAH','JALPAIGURI','KALYANI','KHARAGPUR','MUKANDAPUR','NEW ALIPUR','PORT BLAIR','RAJARHAT','RANAGHAT','RISHRA','SALT LAKE','SILIGURI'}
-SIS_ID =    [ 
-            "SDAS-11","IHNAB04","IHNFH02","BCHAT02","ACHAK02",
-            "JMAJU03","SPKUM02","PUSHP03","ASIL-01 ","BGIRI01",
-            "TDAS01","RDEBN01","NGHOS01","AGHOS02","ASAH-01"]
-SIS_PASSWORD =  [
-    "Air@S77857","AIR@NA7128","AIR@NF4596","Air@K59380","Air@M11211",
-    "Air@J95342","Air@UM7550","Air@P54824","Air@A69931","AIR@BG6137",
-    "AIR@TD9338","Air@P62212","Air@R33447","Air@A34500","Air@A41439"]
 
+SIS_ID =    [ 
+             "******","******","******","******","******", #id s are hidden for security purpose
+             "******","******","******","******","******",
+             "******","******","******","******","******",]
+SIS_PASSWORD =  [
+            "******","******","******","******","******", #passwords as well
+             "******","******","******","******","******",
+             "******","******","******","******","******",]
 
 buttons = []
 row_count = 0
@@ -306,11 +268,6 @@ additional_buttons_frame.pack()
 
 additional_buttons = [ "Quick Data 😎","Move","Merge"]
 
-# for name in additional_buttons:
-# button = tk.Button(additional_buttons_frame, text=name, bg="#A5CFE3", fg="black",font=Font(family="Times New Roman"), command=lambda btn=name: AfterDownloadMoveToFolder())
-# button.pack(side=tk.LEFT, padx=5, pady=5)
-# buttons.append(button)
-
 button = tk.Button(additional_buttons_frame, text=additional_buttons[int(0)], bg="#A5CFE3", fg="black",font=Font(family="Times New Roman"), command=sotreDataWithForLoop)
 button.pack(side=tk.LEFT, padx=5, pady=5)
 buttons.append(button)
@@ -322,14 +279,7 @@ buttons.append(button1)
 button2 = tk.Button(additional_buttons_frame, text=additional_buttons[int(2)], bg="#A5CFE3", fg="black",font=Font(family="Times New Roman"), command=excelCombine)
 button2.pack(side=tk.LEFT, padx=5, pady=5)
 buttons.append(button2)
-# Create a label at the bottom
-# label_text = "© Copyright to Infinite Solutions Inc."
-# label = tk.Label(root, text=label_text, bg="#D3F4FB", fg="black", font=Font(family="Designed & developed by Infinity", size=8,slant="italic"), anchor=tk.E)
-# label.pack(pady=10)
-# label_text2 = "Designed & developed by Infinity"
-# label2 = tk.Label(root, text=label_text2, bg="#D3F4FB", fg="black", font=Font(family="Arial", size=8,slant="italic"), justify="right",anchor="e")
-# label2.pack()
+
 Label(root, text= "© Copyright to Infinite Solutions Inc.                                                                                                      Designed & developed by Infinity", font=Font(family="Arial", size=8,slant="italic"), background="#D3F4FB").pack(pady=0, side= BOTTOM, anchor="e")
-# Label(root, text= " ",font=Font(family="Arial", size=8,slant="italic"), background="#D3F4FB").pack(pady=0, side= BOTTOM, anchor="e")
 
 root.mainloop()
